c14c1e683130c6e39050a099c8d6636a691e3776
Prantik M <111644103+prantikm07@users.noreply.github.com>
2026-01-29 14:58:20 +0530

    Add Test class with addition method

---
+++ test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+  static void add(){
+
+    int a=50;
+    int b=30;
+    int result = a+b;
+    System.out.println("The sum is " +result);
+    
+  }
+  public static void main(String[] args)
+  {
+    add();
+  }
+}
