5c1da7d840e8bc5073bb39a70a4d4327a293c202
Prantik M <111644103+prantikm07@users.noreply.github.com>
2026-01-29 15:05:26 +0530

    Change subtraction to multiplication in add method

--- test.java
+++ test.java
@@ -3,8 +3,8 @@ public class test {
   static void add(){
 
     int a=1000;
-    int b=300;
-    int result = a-b;
+    int b=30;
+    int result = a*b;
     System.out.println("The sum is " +result);
     
   }
